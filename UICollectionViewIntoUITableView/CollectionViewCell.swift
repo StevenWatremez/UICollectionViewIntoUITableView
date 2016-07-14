268dd54e7527fc7b4a8cf272c775923935644b96
Steven Watremez <steven.watremez@niji.fr>
2016-07-14 19:04:42 +0200

    create cells and data

---
+++ UICollectionViewIntoUITableView/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  UICollectionViewIntoUITableView
+//
+//  Created by Steven_WATREMEZ on 14/07/16.
+//  Copyright © 2016 Steven_WATREMEZ. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ UICollectionViewIntoUITableView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  UICollectionViewIntoUITableView
+//
+//  Created by Steven_WATREMEZ on 14/07/16.
+//  Copyright © 2016 Steven_WATREMEZ. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
